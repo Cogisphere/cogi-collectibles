228782f8a7764c4c8a8e43d5a1aa925ff2202294
Pawel <pawel.kuznik@gmail.com>
2022-01-08 12:15:30 +0100

    More setup to turn this into an actual package.

---
+++ cogi-collectibles.ts
@@ -0,0 +1,3 @@
+export { default as Model, ModelState } from "./lib/Structures/Model";
+export { default as buildModel } from "./lib/Actions/buildModel";
+export { default as buildModels } from "./lib/Actions/buildModels";
