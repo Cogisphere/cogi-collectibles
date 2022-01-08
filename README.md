fc46bcf30e794c8d2721d3aca75481c236a414bf
Pawel <pawel.kuznik@gmail.com>
2022-01-08 11:49:22 +0100

    Initial setup

---
+++ README.md
@@ -0,0 +1,3 @@
+# cogi-collectibles
+
+This is a library with logic to define collection of models for cogi-application.
---
+++ lib/Structures/Model.ts
@@ -0,0 +1,38 @@
+
+/**
+ *  Different states that a model can be in.
+ */
+export enum ModelState {
+    Packaged = 'packaged',
+    Assembled = 'assembled',
+    Primed = 'primed',
+    Painted = 'painted',
+    Done = 'done',
+    Broken = 'broken'
+};
+
+/**
+ *  This is an interface describing a model in ones collection.
+ */
+export default interface Model {
+
+    /**
+     *  The unique identifier of the model.
+     */
+    id:string;
+
+    /**
+     *  The name of the model.
+     */
+    name:string;
+
+    /**
+     *  The current model state.
+     */
+    state:ModelState;
+
+    /**
+     *  Notes about the model. This can be MD format.
+     */
+    notes:string;
+};
