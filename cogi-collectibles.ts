47e163cfc95c21912be08c812445b67027e03e6f
Pawel <pawel.kuznik@gmail.com>
2022-01-10 00:35:52 +0100

    Updated exports.

--- cogi-collectibles.ts
+++ cogi-collectibles.ts
@@ -1,3 +1,6 @@
 export { default as Model, ModelState } from "./lib/Structures/Model";
+export { default as Box, BoxAvailability } from "./lib/Structures/Box";
+export { default as PossibleModel } from "./lib/Structures/PossibleModel";
 export { default as buildModel } from "./lib/Actions/buildModel";
 export { default as buildModels } from "./lib/Actions/buildModels";
+export { default as buildBox } from "./lib/Actions/buildBox";
